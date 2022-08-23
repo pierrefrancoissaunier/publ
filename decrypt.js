f82be490cab02155497929a7ba04f1059d57c4cd
pierrefrancoissaunier <56261508+pierrefrancoissaunier@users.noreply.github.com>
2022-08-23 15:46:02 +0200

    Add files via upload

---
+++ decrypt.js
@@ -0,0 +1,8 @@
+export function decrypt(message){
+    let crypted;
+    for (let i = 0; i < message.length; i++) {
+        crypted = crypted + String.fromCharCode(message.charCodeAt(i)-1);
+    }
+
+    return(crypted)
+};
